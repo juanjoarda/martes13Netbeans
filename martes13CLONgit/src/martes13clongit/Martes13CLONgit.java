6d20d72f5afee4e39210c7c0048c9de620127728
juanjoarda <juanjoarda@gmail.com>
2018-03-13 09:08:38 +0100

    actualizacion 1 clon juanjo (13/03/2018)

---
+++ martes13CLONgit/src/martes13clongit/Martes13CLONgit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package martes13clongit;
+
+/**
+ *
+ * @author juanjoadmin
+ */
+public class Martes13CLONgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola, Soy tu padre");
+        // TODO code application logic here
+    }
+    
+}
